473edcadf9fed8e931dfb88d7e06c2cb3ec198e4
Fernando <steelco1326@gmail.com>
2022-07-27 00:21:04 -0500

    commit

--- js/scripts.js
+++ js/scripts.js
@@ -74,50 +74,7 @@ $('.burguer').click(function () {
   $('.burguer').toggleClass('active');
 });
 
-//funcion para cambiar de color el nav cuando pasa la clase "ancla"
 
-var lastScrollTop = 0;
-$(window).scroll(function (e) {
-  $altoportada = $('.ancla').height();
-  var st = $(this).scrollTop();
-  if (st > $altoportada) {
-    $('nav').addClass('activo')
-  } else {
-    $('nav').removeClass('activo')
-  }
-  if ($(".responsive-menu").hasClass("activo")) {
-    $('nav').addClass('scrolldown')
-  }
-  lastScrollTop = st;
-
-});
-
-/***** empieza timeline para los iconos ******/
-
-let tmline = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".objetivos",
-    toggleActions: "play none none none",
-    start: "top center"
-    //markers: true
-  }
-});
-
-tmline.from(".icon", {
-  opacity: 0,
-  scale: 0,
-  duration: .8
-});
-
-tmline.from(".puntos", {
-  rotation: "360",
-  transformOrigin: '50% 50%',
-  repeat: -1,
-  duration: 15,
-  ease: "linear"
-});
-
-/***** ANIMACION PARA TODa la pagina ******/
 
 const textos = gsap.utils.toArray('.text');
 textos.forEach(text => {
@@ -136,38 +93,6 @@ textos.forEach(text => {
     }
   })
 });
-
-
-const underlines = gsap.utils.toArray('.underlineChild');
-underlines.forEach(underline => {
-  gsap.to(underline, {
-    width:"95%",
-    ease: Power4.easeOut,
-    duration: 2,
-    delay:.6,
-    scrollTrigger: {
-      trigger: underline,
-      toggleActions: "restart none none reverse",
-      start: "top 80%",
-      //markers:true
-    }
-  })
-});
-
-
-gsap.from('.width', {
-  width:"0",
-  padding:"0",
-  ease: Power4.easeOut,
-  duration: .5,
-  //delay:.5,
-  scrollTrigger: {
-    trigger: '.consumidores3' ,
-    toggleActions: "restart none none reverse",
-    start: "top 80%",
-    //markers:true
-  }
-})
 
 $(".coloredbg").each(function() {
   // Content Reveal Animation
@@ -197,33 +122,6 @@ $('.faq li').click(function () {
   $(this).toggleClass('active');
 })
 
-
-$('#switch1').click(function () {
-  $(this).find('.sd').toggleClass('active');
-  $('.av1').toggleClass('active');
-  $('.light1').toggleClass('active');
-})
-
-$('#switch2').click(function () {
-  $(this).find('.sd').toggleClass('active');
-  $('.av2').toggleClass('active');
-  $('.light2').toggleClass('active');
-})
-
-$('#switch3').click(function () {
-  $(this).find('.sd').toggleClass('active');
-  $('.av3').toggleClass('active');
-  $('.light3').toggleClass('active');
-})
-
-
-
-
-/////////////////////////// 3js
-
-
-
-
 /////////////////////////// modal
 window.onload = function () {
   modal.style.display = "block";
@@ -241,47 +139,6 @@ window.onload = function () {
     modal.style.display = "none";
   }
 };
-
-var modal = document.getElementById('myModal');
-
-var lastScrollTop = 0;
-$(window).scroll(function (e) {
-  $altoportada = $('.ancla').height();
-  var st = $(this).scrollTop();
-  if (st > lastScrollTop && st > $altoportada) {
-    $('nav').removeClass('scrolldown')
-  } else {
-    $('nav').addClass('scrolldown')
-  }
-  if ($(".responsive-menu").hasClass("activo")) {
-    $('nav').addClass('scrolldown')
-  }
-  lastScrollTop = st;
-  resizeMenn()
-});
-
-$(window).resize(function () {
-  resizeMenn()
-});
-
-function resizeMenn() {
-  $width = $(window).width();
-  if ($width > 700) {
-    if ($(this).scrollTop() > 1000) {
-      $('.nav-text').removeClass('active');
-      $('.nav-text').addClass('inactive');
-      $('.nav-icon').addClass('active');
-    } else {
-      $('.nav-text').addClass('active');
-      $('.nav-text').removeClass('inactive');
-      $('.nav-icon').removeClass('active');
-    }
-  } else {
-  }
-}
-resizeMenn()
-
-
 
 /////////////////////////// TYPED JS
 
